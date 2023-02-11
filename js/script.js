f3fb7face3e390ea7b2e650c6c11e9be2382f8c6
Facu Areosa <facundo.areosa@gmail.com>
2023-02-11 21:33:27 +0100

    Arreglado el cierre del nav/Agregado los textos

--- js/script.js
+++ js/script.js
@@ -8,3 +8,12 @@ window.addEventListener("resize", function() {
     linkComentarios.href = "https://www.booking.com/hotel/es/the-boathouse.es.html?auth_success=1&activeTab=htReviews";
   }
 });
+
+// CERRAR NAVBAR AL HACER CLICK EN UN ELEMENTO
+
+document.addEventListener("click", function(event) {
+    const navbarCollapse = document.querySelector(".navbar-collapse");
+    if (event.target.matches(".navbar-collapse.show a:not(.dropdown-toggle)")) {
+      navbarCollapse.classList.remove("show");
+    }
+  });
